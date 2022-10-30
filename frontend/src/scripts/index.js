f0a304eff49571d0c77319f0d372861a199a3ed0
LYK <dalinaum@gmail.com>
2022-10-30 22:08:35 +0900

    HMR 연동

--- frontend/src/scripts/index.js
+++ frontend/src/scripts/index.js
@@ -4,5 +4,5 @@ import $ from "jquery/dist/jquery.slim";
 import "bootstrap/dist/js/bootstrap.bundle";
 
 $(document).ready(() => {
-    window.console.log("dom ready");
+    window.console.log("dom ready from michaelyin");
 });
--- frontend/webpack/webpack.config.dev.js
+++ frontend/webpack/webpack.config.dev.js
@@ -9,14 +9,22 @@ const common = require('./webpack.common.js');
 const { truncate } = require('fs');
 
 module.exports = merge(common, {
+  target: 'web',
   mode: 'development',
   devtool: 'eval-cheap-source-map',
   output: {
     chunkFilename: 'js/[name].chunk.js',
+    publicPath: 'http://localhost:9091/',
   },
   devServer: {
-    inline: true,
     hot: true,
+    port: 9091,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    devMiddleware: {
+      writeToDisk: true,
+    },
   },
   plugins: [
     new Webpack.DefinePlugin({
